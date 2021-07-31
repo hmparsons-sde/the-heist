5d43e4d38e8979a1ee690fb9c840715f8c622b6a
Holly Parsons <holly-parsons@outlook.com>
2021-07-30 19:39:40 -0500

    Add project files.

---
+++ Heist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_heist
+{
+    class Heist
+    {
+        public int BankDifficulty { get; private set; }
+        private int _luckValue { get; set; }
+
+        public Heist()
+        {
+            Console.WriteLine("Enter the difficulty of the bank as an integer");
+            BankDifficulty = int.Parse(Console.ReadLine());
+
+            var crew = new Crew();
+
+            Console.WriteLine("Enter the number of times to run the scenario");
+            var trials = int.Parse(Console.ReadLine());
+            var successes = 0;
+            var failures = 0;
+            for (var i = 0; i < trials; i++)
+            {
+                Random r = new Random();
+                _luckValue = r.Next(-10, 10);
+                var adjustedDifficulty = BankDifficulty + _luckValue;
+                if (crew.CumulativeSkill < adjustedDifficulty)
+                {
+                    failures++;
+                }
+                else
+                {
+                    successes++;
+                }
+            }
+            Console.WriteLine($"{crew.Name} successfully heisted {successes} out of {trials} runs and failed {failures} out of {trials} runs");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace the_heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+            var heist = new Heist();
+        }
+    }
+}
